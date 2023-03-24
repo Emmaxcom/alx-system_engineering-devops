205dd4e396343d748eb986b73d2b45650a817416
Emmaxcom <emmaxcom@yahoo.com>
2023-03-24 09:43:42 -0700

    configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,5 @@
-#creates a file
-file {'/tmp/holberton':
+# Create a file called holberton in the /tmp directory
+file { '/tmp/holberton':
   ensure  => file,
   path    => '/tmp/holberton',
   mode    => '0744',
