00dd19ad1550483c0b7ab47ab9d7c3a040c279dc
Emmaxcom <emmaxcom@yahoo.com>
2023-03-24 09:45:55 -0700

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-#install pupplet lint
+# Install puppet-lint
 package { 'puppet-lint':
-  ensure   => '1.1.0',
-  provider => 'gem',
+  ensure   => 'latest',
+  name     => 'puppet-lint',
+  provider => 'gem'
 }
